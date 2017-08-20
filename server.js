858bee7e59b61db075512515669b09ecf26124a7
Jon Oliver <imjonoliver@gmail.com>
2017-08-20 16:05:35 -0400

    Add a buncha stuff

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var io = require('socket.io')();
 console.log('running');
 io.on('connection', function(socket){
   console.log('connected');
-  socket.on('event', function(data){
+  socket.on('update', function(data){
     socket.broadcast.to('room').emit('update', data)
+  });
+  socket.on('tap', function(data){
+    socket.broadcast.to('room').emit('tap', data)
   });
   socket.join('room');
   socket.on('disconnect', function(){});
