1fa47c0dec569b0314908ea49a5c0b8930064f5d
Jon Oliver <imjonoliver@gmail.com>
2017-08-21 22:11:16 -0400

    Add some comments

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-var replace = require('stream-replace');
-const Koa = require('koa');
-const app = new Koa();
-
 const HOST = process.env.HOST || 'http://192.168.0.6';
 const PORT = process.env.PORT || 3000;
 const ROOTPATH = `http://${HOST}:${PORT}`;
 
+// Create app server
+const Koa = require('koa');
+const app = new Koa();
+
+// Configure server //
+
+// Reload connected browsers when public files change
 app.use(require('koa-browser-sync')({
   init: true,
   files: 'public/*',
   notify: false
 }));
 
+// Text transformations for static files
+// $SOCKET_SERVER is the host/port that socket io client is running
+const replace = require('stream-replace');
 app.use(async (ctx, next) => {
   await next();
   if (!ctx.body) { return; }
   ctx.body = ctx.body.pipe(replace(/\$SOCKET_SERVER/g, ROOTPATH));
 });
 
+// serve static files from public
 app.use(require('koa-static')('public/', {defer: true}));
 
+// configure socket connection
 const IO = require( 'koa-socket' );
 const io = new IO();
 io.attach( app );
@@ -35,5 +43,6 @@ app._io.on( 'connection', socket => {
   socket.on('disconnect', function(){});
 })
 
+// Start 'er up!
 app.listen(PORT);
 console.log(`Listening on ${ROOTPATH}`);
